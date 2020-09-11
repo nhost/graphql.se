50cdebd7bf3651d8290e4c49d0c840ed9a11b95d
elitan <johan@eliasson.me>
2020-09-11 10:48:05 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Head from "next/head";
 
 export default function Home() {
-  return <div>we are live. GraphQL Sweden</div>;
+  return (
+    <div>
+      <div>We are live. GraphQL 🇸🇪!</div>
+      By <a href="https://nhost.io">Nhost</a>.
+    </div>
+  );
 }
