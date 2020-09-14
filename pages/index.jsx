629ccfb21a6bfe1e578fc255dba2fd3d2ea158f6
elitan <johan@eliasson.me>
2020-09-14 20:33:35 +0200

    update

---
+++ pages/index.jsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="container mx-auto flex-grow">
+        <div className="text-6xl font-bold text-center leading-none text-graphql py-10">
+          GraphQL.se Meetup Stockholm
+        </div>
+        <div className="text-3xl font-bold text-gray-800 text-center py-6">
+          An pretty good meetup. Don't miss it!
+        </div>
+        <div className="w-full flex items-center justify-center py-6 uppercase font-bold text-gray-600">
+          <div>21 October 2020</div>
+          <div className="px-2">|</div>
+          <div>WeWork, Stockholm</div>
+        </div>
+      </div>
+      <div className="text-center py-4">
+        By{" "}
+        <a href="https://nhost.io" className="text-graphql">
+          Nhost
+        </a>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "accent-1": "#333",
+        graphql: "#D64292",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
