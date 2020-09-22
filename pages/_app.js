932644a68e3be598cd4c1b0256a7d5c653ac07ef
elitan <johan@eliasson.me>
2020-09-22 11:59:53 +0200

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "../styles/index.css";
 import { graphqlEndpoint } from "utils/config";
 import { NhostApolloProvider } from "react-nhost";
@@ -5,6 +6,9 @@ import { NhostApolloProvider } from "react-nhost";
 function MyApp({ Component, pageProps }) {
   return (
     <NhostApolloProvider gqlEndpoint={graphqlEndpoint}>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </NhostApolloProvider>
   );
